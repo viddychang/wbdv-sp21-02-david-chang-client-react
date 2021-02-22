d52dc895947e129b0be84c3270f910e43c93c91f
dchang11194 <dchang11194@gmail.com>
2021-02-21 23:39:18 -0500

    course-card and grid are done

--- src/components/course-card.js
+++ src/components/course-card.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react'
 import {Link} from "react-router-dom";
 
-const CourseCard = ({deleteCourse, updateCourse, course}) => {
+const CourseCard = ({deleteCourse, updateCourse, course, title}) => {
     const [editing, setEditing] = useState(false)
-    const [newTitle, setNewTitle] = useState(course.title)
+    const [newTitle, setNewTitle] = useState(title)
 
     const saveTitle = () => {
         setEditing(false)
@@ -14,37 +14,36 @@ const CourseCard = ({deleteCourse, updateCourse, course}) => {
         updateCourse(newCourse)
     }
 
+    const deleteTitle = () => {
+        setEditing(false)
+        deleteCourse(course)
+    }
+
     return (
       <div className="col-sm-6 col-md-4 col-lg-3">
         <div className="card">
           <div className="card-body">
           {
-            editing && <div className="text-right">
-                <i onClick={() => saveTitle()} className="fas fa-check color-green"></i>
-                <i onClick={() => {setEditing(false) return deleteCourse(course)}}
-                    className="fas fa-times color-red"></i>
-             </div>
-          }
-          {
             !editing && <h5 className="card-title">{title}</h5>
           }
           {
-            editing && <input onChange={(event => {setNewTitle(event.target.value)})}
-                            className="form-control"
-                            value={newTitle}/>
+            editing && <input onChange={(event) => setNewTitle(event.target.value)}
+                              value={newTitle}
+                              className="form-control"/>
           }
+
           <p className="card-text">
             {course.description}
           </p>
           <Link to="/courses/editor" className="btn btn-primary">
             {course.title}
           </Link>
-          <div className="card-foot">
             <div className="float-right">
-                {!editing && <i onClick{() => setEditing(true)}
-                    classname="fas fa-pencil-alt"></i>}
-                }
+                {!editing && <i onClick={() => setEditing(true)} className="fas fa-2x fa-edit"></i>}
+                {editing && <i onClick={() => saveTitle()} className="fas fa-2x fa-check"></i>}
+                {editing && <i onClick={() => deleteTitle()} className="fas fa-2x fa-trash"></i>}
             </div>
+            
           </div>
         </div>
       </div>
--- src/components/course-grid.js
+++ src/components/course-grid.js
@@ -2,18 +2,42 @@ import React from 'react'
 import CourseCard from "./course-card";
 import {Link} from "react-router-dom";
 
-const CourseGrid = ({courses}) =>
+const CourseGrid = ({courses, deleteCourse, addCourse, updateCourse}) =>
   <div>
-      <Link to="/courses/table">
-        <i className="fas fa-list fa-2x float-right"></i>
-      </Link>
-    <h2>Course Grid {courses.length}</h2>
+    <div className="row"> 
+      <div className="col-4 d-none d-sm-none d-md-block">
+        <h4>
+          Recent Documents  
+        </h4> 
+      </div>
+      <div className="col-4 d-none d-sm-none d-md-block">
+        <h4>
+          Owned by me
+        </h4>
+      </div>
+      <div className="col-4">
+        <span className="float-right">
+          <i className="fas fa-2x fa-folder"></i>
+          <i className="fas fa-2x fa-sort"></i>
+          <Link to="/courses/table">
+            <i className="fas fa-list fa-2x"></i>
+          </Link>
+        </span>
+      </div>
+    </div>
     <div className="row">
-    {
-      courses.map(course =>
-        <CourseCard course={course}/>
-      )
-    }
+      
+
+    </div>
+    <div className="row">
+      {
+        courses.map(course =>
+          <CourseCard course={course}
+                      deleteCourse={deleteCourse}
+                      updateCourse={updateCourse}
+                      title={course.title}/>
+        )
+      }
     </div>
   </div>
 
