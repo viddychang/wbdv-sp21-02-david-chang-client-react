a48f9c27e21d5912b6592421862235f219e7026a
dchang11194 <dchang11194@gmail.com>
2021-04-18 10:38:39 -0400

    assignment8

--- src/components/quizzes/quiz.js
+++ src/components/quizzes/quiz.js
@@ -2,17 +2,14 @@ import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import Question from "./questions/question";
 import '../styles.css';
+import quizzesService from "../../services/"
 
 const Quiz = () => {
     const {quizId} = useParams()
     const [questions, setQuestions] = useState([])
     useEffect(() => {
         // TODO: move this to a service file
-        fetch(`http://localhost:4000/api/quizzes/${quizId}/questions`)
-            .then(response => response.json())
-            .then((questions) => {
-                setQuestions(questions)
-            })
+        setQuestions(quizzesService.findAllQuizzes())
     }, [])
 
     return(
