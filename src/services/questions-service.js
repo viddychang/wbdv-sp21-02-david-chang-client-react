da8e6a2381888bcb9dc367d9316ba06876e02b46
dchang11194 <dchang11194@gmail.com>
2021-04-18 09:52:38 -0400

    assignment8. mongodb set up with data

--- src/services/questions-service.js
+++ src/services/questions-service.js
@@ -1,4 +1,8 @@
-const QUIZZES_URL = 'http://localhost:4000/api/quizzes';
+//const QUIZZES_URL = 'http://localhost:4000/api/quizzes';
+
+const QUIZZES_URL = 'https://wbdv-sp21-02-dc-server-node.herokuapp.com/api/quizzes';
+
+
 const findQuestionsForQuiz = (qid) => {
    return fetch(`${QUIZZES_URL}/${qid}/questions`)
       .then(response => response.json())
--- src/services/quizzes-service.js
+++ src/services/quizzes-service.js
@@ -1,4 +1,8 @@
-const QUIZZES_URL = 'http://localhost:4000/api/quizzes';
+
+//const QUIZZES_URL = 'http://localhost:4000/api/quizzes';
+
+const QUIZZES_URL = 'https://wbdv-sp21-02-dc-server-node.herokuapp.com/api/quizzes';
+
 const findAllQuizzes = () => {
    return fetch(QUIZZES_URL)
       .then(response => response.json())
@@ -7,6 +11,19 @@ const findQuizById = (qid) => {
    return fetch(`${QUIZZES_URL}/${qid}`)
       .then(response => response.json())
 }
+
+const submitQuiz = (quizId, questions) => {
+   fetch(`${QUIZZES_URL}/${quizId}/attempts`, {
+     method: 'POST',
+     body: JSON.stringify(questions),
+     headers: {
+       'content-type': 'application/json'
+     }
+   }).then(response => response.json())
+     .then(result => console.log(result))
+  }
+  
+
 export default {
-   findAllQuizzes, findQuizById
+   findAllQuizzes, findQuizById, submitQuiz
 }
